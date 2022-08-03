9eec721c1ee95752a3e90d94c41b5a3666fa4954
Michael <michel.abu.n@gmail.com>
2022-08-03 12:42:40 +0300

    second spush and commit

--- app/src/main/java/com/example/michel_house/activities/MainActivity.java
+++ app/src/main/java/com/example/michel_house/activities/MainActivity.java
@@ -39,10 +39,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         imageView = findViewById(R.id.iconm1);
         progressBar = findViewById(R.id.progressbar);
         loadImages("iconsec.png");
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
